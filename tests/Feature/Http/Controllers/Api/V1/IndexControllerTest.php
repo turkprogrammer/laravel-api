d537f48e2680710f5917eae3945205213ca48fc2
turkprogrammer <ostbat43@gmail.com>
2023-02-04 21:07:42 +0500

    Unit tests for IndexController

--- tests/Feature/Http/Controllers/Api/V1/IndexControllerTest.php
+++ tests/Feature/Http/Controllers/Api/V1/IndexControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Api\V1\IndexController;
+use App\Http\Resources\VersionResource;
 use Tests\TestCase;
 
 /**
@@ -49,10 +50,72 @@ final class IndexControllerTest extends TestCase
     /**
      * @return void
      */
+    public function testIndexReturnsVersionResource()
+    {
+        $response = $this->get('/api/version');
+
+        $response->assertResource(VersionResource::class);
+    }
+
+    /**
+     * @return void
+     */
+    public function testIndexCachesResponse()
+    {
+        $response = $this->get('/api/version');
+
+        $this->assertTrue($response->headers->hasCacheControlDirective('public'));
+        $this->assertTrue($response->headers->hasCacheControlDirective('max-age'));
+        $this->assertTrue($response->headers->hasCacheControlDirective('s-maxage'));
+    }
+
+    /**
+     * @return void
+     */
+    public function testIndexReturnsLatestVersion()
+    {
+        $response = $this->get('/api/version');
+
+        $response->assertJsonFragment([
+            'version' => '1.0.0'
+        ]);
+    }
+
+    /**
+     * @return void
+     */
+    public function testIndexReturnsExpectedData()
+    {
+        $response = $this->get('/api/version');
+
+        $response->assertJsonFragment([
+            'version' => '1.0.0',
+            'release_date' => '2018-01-01',
+            'release_notes' => 'Initial release'
+        ]);
+    }
+
+    /**
+     * @return void
+     */
     public function testAll(): void
     {
-        /** @todo This test is incomplete. */
-        $this->getJson('/all')
-            ->assertStatus(200);
+        $response = $this->json('GET', '/api/v1/users/all');
+
+        $response->assertStatus(200);
+        $response->assertJson(['status' => true]);
+        $response->assertJsonStructure([
+            'status',
+            'data' => [
+                '*' => [
+                    'id',
+                    'name',
+                    'email',
+                    'created_at',
+                    'updated_at',
+                ]
+            ]
+        ]);
+        $response->assertJsonValidationErrors([]);
     }
 }
